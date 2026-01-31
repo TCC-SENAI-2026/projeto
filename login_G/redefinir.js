4d4d3e52e875d3b6fba3af23dbc5612e56a2f8bf
GabriellySantos00 <gabby28.ap@gmail.com>
2026-01-30 22:00:50 -0300

    modificando JS

--- login_G/redefinir.js
+++ login_G/redefinir.js
@@ -1,20 +1,20 @@
 
-function openModal() {
-  document.getElementById("resetModal").style.display = "flex";
-}
+// function openModal() {
+//   document.getElementById("resetModal").style.display = "flex";
+// }
 
-function closeModal() {
-  document.getElementById("resetModal").style.display = "none";
-}
+// function closeModal() {
+//   document.getElementById("resetModal").style.display = "none";
+// }
 
-function enviarSolicitacao() {
-  alert("Solicitação enviada com sucesso.");
-  closeModal();
-}
+// function enviarSolicitacao() {
+//   alert("Solicitação enviada com sucesso.");
+//   closeModal();
+// }
 
-/* fecha ao clicar fora da caixinha */
-window.addEventListener("click", function (e) {
-  const modal = document.getElementById("resetModal");
-  if (e.target === modal) closeModal();
-});
+// /* fecha ao clicar fora da caixinha */
+// window.addEventListener("click", function (e) {
+//   const modal = document.getElementById("resetModal");
+//   if (e.target === modal) closeModal();
+// });
 
